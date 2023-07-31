11cc4bb0a0dbbefb0db90c4cd2d402bdacd07afc
Thanadole Bunyamark <spikeman147@gmail.com>
2023-07-31 20:57:50 +0700

    2.8

--- real-world-vue/src/views/EventListView.vue
+++ real-world-vue/src/views/EventListView.vue
@@ -11,7 +11,7 @@ const events = ref<EventItem[]>([])
 const totalEvent = ref<number>(0)
 const hasNextPage = computed(() => {
   const totalPages = Math.ceil(totalEvent.value /2)
-  return props.page.valueOf() <totalPages>
+  return props.page.valueOf() <totalPages
 })
 const props = defineProps({
   page: {
@@ -21,8 +21,7 @@ const props = defineProps({
 })
 
 watchEffect(() => {
-  EventService.getEvent(2, props.page)
-  .then((response: AxiosResponse<EventItem[]>) => {
+  EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
     events.value = response.data
     totalEvent.value = response.headers['x-total-count']
     console.log(response.data)
@@ -34,18 +33,23 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Events For Good</h1>
+    <div>
+          <h1>Events For Good</h1>
   <main class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
-    <RouterLink :to="{ name: 'event-list', query: { page: page - 1}} " rel="prev" v-if="page!=1">
+    <div class="pagination">
+      <RouterLink :to="{ name: 'EventList', query: { page: page - 1 } }" rel="prev" v-if="page!=1" id="page-prev">
     Prev Page
     </RouterLink>
-    <RouterLink :to="{ name: 'event-list', query: { page: page + 1}} " rel="prev" v-if="hasNextPage">
+    <RouterLink :to="{ name: 'EventList', query: { page: page + 1 } }" rel="next" v-if="hasNextPage" id="page-next">
     Next Page
     </RouterLink>
+    </div>
+    
   </main>
-  </div>
+    </div>
+
+  
   
 </template>
 
@@ -54,5 +58,21 @@ watchEffect(() => {
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+  
+  .pagination {
+    display: flex;
+    width: 290px;
+  }
+  .pagination a {
+    flex: 1;
+    text-decoration: none;
+    color: #2c3e50;
+  }
+  #page-prev {
+    text-align: left;
+  }
+  #page-next {
+    text-align: right;
   }
 </style>
